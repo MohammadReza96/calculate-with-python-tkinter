95497f912c7aef5689f0c8611c908fc398b1cc31
MohammadReza96 <mohammadrz.yazdani.96@gmail.com>
2023-07-03 01:02:49 +0330

    update project

--- calculate (function)/calculator_versiion_1.py
+++ calculate (function)/calculator_versiion_1.py
@@ -1,11 +1,10 @@
-#programed by Mz96
-
-#-------------------------------------------reqirments
+#programed by Mohammadrz96
+#----------------------------------------------------------- reqirments
 from tkinter import *
-#--------------------------------------optional module
+#----------------------------------------------------------- optional module
 import os 
 os.system('cls')
-#------------------------------------creating functions
+#----------------------------------------------------------- creating functions
 def buttom_click(number):
     current_number=myentry_1.get()
     myentry_1.delete(0,END)
@@ -17,7 +16,10 @@ def plus(event):
     label_1.config(text="plus")
     global n1
     global sign
-    first_number=float(myentry_1.get())
+    try:
+        first_number=float(myentry_1.get())
+    except ValueError:
+        first_number=myentry_1.get()
     n1=first_number
     sign="plus"
     myentry_1.delete(0,END)
@@ -27,7 +29,10 @@ def minus(number):
     label_1.config(text="minus")
     global n1
     global sign
-    first_number=float(myentry_1.get())
+    try:
+        first_number=float(myentry_1.get())
+    except ValueError:
+        first_number=myentry_1.get()
     n1=first_number
     sign="minus"
     myentry_1.delete(0,END)
@@ -37,7 +42,12 @@ def times(number):
     label_1.config(text="times")
     global n1
     global sign
-    first_number=float(myentry_1.get())
+    try:
+        first_number=float(myentry_1.get())
+    except ValueError:
+        first_number=myentry_1.get()
+        
+    # first_number=float(myentry_1.get())
     n1=first_number
     sign="times"
     myentry_1.delete(0,END)
@@ -47,7 +57,11 @@ def divide(number):
     label_1.config(text="divide")
     global n1
     global sign
-    first_number=float(myentry_1.get())
+    try:
+        first_number=float(myentry_1.get())
+    except ValueError:
+        first_number=myentry_1.get()
+    # first_number=float(myentry_1.get())
     n1=first_number
     sign="divide"
     myentry_1.delete(0,END)
@@ -60,15 +74,55 @@ def equal(number):
     secend_number=myentry_1.get()
     myentry_1.delete(0,END)
     if sign=="plus":
-        myentry_1.insert(0,n1+float(secend_number))
+        try:
+            if int(secend_number) and n1=='infinite ...':
+                res='infinite ...'
+            elif int(secend_number) and n1=='impossible ...':
+                res='impossible ...'
+            else:
+                res=n1+float(secend_number)
+        except TypeError:
+            res='impossible ...'
+        myentry_1.insert(0,res)
     elif sign=="minus":
-        myentry_1.insert(0,n1-float(secend_number))
+        try:
+            if int(secend_number) and n1=='infinite ...':
+                res='infinite ...'
+            elif int(secend_number) and n1=='impossible ...':
+                res='impossible ...'
+            else:
+                res=n1-float(secend_number)
+        except TypeError:
+            res='impossible ...'
+        myentry_1.insert(0,res) 
+        
+        
+        # myentry_1.insert(0,n1-float(secend_number))
     elif sign=="times":
-        myentry_1.insert(0,n1*float(secend_number))
+        try:
+            if int(secend_number)==0 and n1=='infinite ...':
+                res=0
+            elif int(secend_number) and n1=='infinite ...':
+                res='infinite ...'
+            elif int(secend_number)==0 and n1=='impossible ...':
+                res='impossible ...'
+            else:
+                res=n1*float(secend_number)
+        except TypeError:
+            res='impossible ...'
+        myentry_1.insert(0,res) 
     else:
-        myentry_1.insert(0,n1/float(secend_number))
+        try:
+            res=n1/float(secend_number)
+        except ZeroDivisionError:
+            res='infinite ...'
+        except ValueError:
+            res='impossible ...'
+        except TypeError:
+            res='impossible ...'
+        myentry_1.insert(0,res)
     sign=""
-#--------------------------------------------------------------clear
+#----------------------------------------------------------- clear
 def clear(event):  
     label_1.config(text="clear")
     myentry_1.delete(0,END)
@@ -113,7 +167,7 @@ def func9(event):
     button_9.config(bg="#F18F01",fg="#000000")
 def func99(event):
     button_9.config(bg="#f0f0f0",fg="#000000")
-#------------------------------------------------------------- + / - / * / "/" / clear / equal    enter & leave ravadid
+#----------------------------------------------------------- + / - / * / "/" / clear / equal    enter & leave ravadid
 def funcp(event):
     button_plus.config(bg="#F18F01",fg="#000000")
 def funcp1(event):
@@ -138,14 +192,14 @@ def funcc(event):
     button_clear.config(bg="#F18F01",fg="#000000")
 def funcc1(event):
     button_clear.config(bg="#f0f0f0",fg="#000000")
-#-------------------------------------------------body of calculator
+#----------------------------------------------------------- body of calculator
 root=Tk()
 myentry_1=Entry(root,borderwidth=5,width=35)
 root.title("Calculator (Ver1)")
 root.resizable(width=False,height=False)
-#---------------------------------------------------
+#-----------------------------------------------------------
 label_1=Label(root,text="cacio",fg="#000000",padx=10,pady=5)
-#-----------------------------------------------number button making
+#----------------------------------------------------------- number button making
 button_1=Button(root,text=1,width=4,padx=20,pady=10)
 button_2=Button(root,text=2,width=4,padx=20,pady=10)
 button_3=Button(root,text=3,width=4,padx=20,pady=10)
@@ -156,7 +210,7 @@ button_7=Button(root,text=7,width=4,padx=20,pady=10)
 button_8=Button(root,text=8,width=4,padx=20,pady=10)
 button_9=Button(root,text=9,width=4,padx=20,pady=10)
 button_0=Button(root,text=0,width=4,padx=20,pady=10)
-#---------------------------------------------- +/-/'/'/*/clear button making
+#----------------------------------------------------------- +/-/'/'/*/clear button making
 button_clear=Button(root,text="clear",width=4,padx=20,pady=10)
 button_equal=Button(root,text="=",width=4,padx=20,pady=10)
 button_plus=Button(root,text='+',width=4,padx=20,pady=10)
@@ -164,7 +218,7 @@ button_minus=Button(root,text='-',width=4,padx=20,pady=10)
 button_divide=Button(root,text='/',width=4,padx=20,pady=10)
 button_times=Button(root,text='*',width=4,padx=20,pady=10)
 myentry_1.grid(row=0,column=0,columnspan=3,padx=5,pady=5)
-#-----------------------------------------------number box griding
+#----------------------------------------------------------- number box griding
 button_1.grid(row=1,column=0)
 button_2.grid(row=1,column=1)
 button_3.grid(row=1,column=2)
@@ -175,24 +229,24 @@ button_7.grid(row=3,column=0)
 button_8.grid(row=3,column=1)
 button_9.grid(row=3,column=2)
 button_0.grid(row=4,column=0)
-#----------------------------------------------clear/= box griding
+#----------------------------------------------------------- clear/= box griding
 button_clear.grid(row=4,column=1)
 button_equal.grid(row=4,column=2)
-#--------------------------------------------- +/-/*/'/'/ box griding
+#-----------------------------------------------------------  +/-/*/'/'/ box griding
 button_plus.grid(row=1,column=3)
 button_minus.grid(row=2,column=3)
 button_times.grid(row=3,column=3)
 button_divide.grid(row=4,column=3)
-#----------------------------------------------entry box griding
+#----------------------------------------------------------- entry box griding
 label_1.grid(row=0,column=3)
-#--------------------------------------------- +/-/*/'/'//=/clear/ binding
+#----------------------------------------------------------- +/-/*/'/'//=/clear/ binding
 button_clear.bind("<Button>",clear)
 button_plus.bind("<Button>",plus)
 button_minus.bind("<Button>",minus)
 button_times.bind("<Button>",times)
 button_divide.bind("<Button>",divide)
 button_equal.bind("<Button>",equal)
-#----------------------------------------------number clicking binding
+#----------------------------------------------------------- number clicking binding
 button_1.bind("<Button>",lambda x :buttom_click(1))
 button_2.bind("<Button>",lambda x :buttom_click(2))
 button_3.bind("<Button>",lambda x :buttom_click(3))
@@ -203,7 +257,7 @@ button_7.bind("<Button>",lambda x :buttom_click(7))
 button_8.bind("<Button>",lambda x :buttom_click(8))
 button_9.bind("<Button>",lambda x :buttom_click(9))
 button_0.bind("<Button>",lambda x :buttom_click(0))
-#----------------------------------------------------optional functions
+#----------------------------------------------------------- optional functions
 button_0.bind("<Enter>",func0)
 button_0.bind("<Leave>",func00)
 button_1.bind("<Enter>",func1)
@@ -224,7 +278,7 @@ button_8.bind("<Enter>",func8)
 button_8.bind("<Leave>",func88)
 button_9.bind("<Enter>",func9)
 button_9.bind("<Leave>",func99)
-#------------------------------------------------
+#-----------------------------------------------------------
 button_plus.bind("<Enter>",funcp)
 button_plus.bind("<Leave>",funcp1)
 button_minus.bind("<Enter>",funcm)
